c9203a6dcba8128a4f57236853579ebf97dd3709
Crazer <Crazer@DESKTOP-2T0SI9E>
2024-03-27 11:49:07 +0530

    Demo Class Committed

---
+++ src/test/java/Practice/GitPractice_1.java
@@ -0,0 +1,13 @@
+package Practice;
+
+public class GitPractice_1 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hi Man");
+		System.out.println("Hello World");
+		
+	}
+
+}
