62143cb24137fd0fba20c83093283407a71b8e68
Crazer <Crazer@DESKTOP-2T0SI9E>
2024-03-28 11:07:16 +0530

    ObjectRepoCommit

---
+++ src/main/java/ObjectRepo/LoginSample.java
@@ -0,0 +1,18 @@
+package ObjectRepo;
+
+
+public class LoginSample
+{
+			//Rule 1 : Create a separate POM class every WebPage
+			
+			//Rule 2 : identify the web element by using @FindBy and make them private
+			
+			
+			//Rule 3 : Create a Constructor for initialization 
+			
+			
+			
+			
+			
+			
+}
